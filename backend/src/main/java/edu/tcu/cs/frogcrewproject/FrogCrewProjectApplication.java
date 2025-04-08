60c5de5b385ed2618839aac4b27a89a42b9c2131
okayamaya24 <amaya.harris@tcu.edu>
2025-04-08 17:22:36 -0500

    Add files via upload

---
+++ backend/src/main/java/edu/tcu/cs/frogcrewproject/FrogCrewProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogCrewProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogCrewProjectApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/edu/tcu/cs/frogcrewproject/FrogCrewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcrewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogCrewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
