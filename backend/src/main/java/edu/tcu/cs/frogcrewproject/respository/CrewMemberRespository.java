ebd9370e0c37603a417ceab64a60d72ea652498b
okayamaya24 <amaya.harris@tcu.edu>
2025-04-09 16:44:12 -0500

    Delete backend directory

--- backend/src/main/java/edu/tcu/cs/frogcrewproject/respository/CrewMemberRespository.java
+++
@@ -1,7 +0,0 @@
-package edu.tcu.cs.frogcrewproject.respository;
-
-import edu.tcu.cs.frogcrewproject.model.CrewMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrewMemberRespository extends JpaRepository<CrewMember, Long> {
-}
