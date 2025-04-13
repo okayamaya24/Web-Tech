c4d9659f42aa339e4832660ba6f476a5cc1b8d29
mycoro <mayracoronilla9@gmail.com>
2025-04-13 16:06:40 -0500

    Starting Use Case 18, admin creating game schedule, just added necessary html elements

---
+++ frontend/src/components/ScheduleBuilder.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="schedule">
+        <table>
+            <thead>
+                <tr>
+                    <th>Sport Type</th>
+                    <th>Game Date and Time</th>
+                    <th>Venue</th>
+                    <th>Opponent (If Applicable)</th>
+                    <th>Required Crew Positions</th>
+                </tr>
+                <!-- must have one game entry to be able to submit -->
+                 <!-- need to work on how user can input information within a table -->
+                <tr> 
+                    <td></td>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style>
+</style>
