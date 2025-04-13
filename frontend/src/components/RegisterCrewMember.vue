86f5f0619c00b0294fe92deb9be340a38183488c
mycoro <mayracoronilla9@gmail.com>
2025-04-12 19:16:22 -0500

    Finished Use Case 1 Confirming User Input Function

--- frontend/src/components/RegisterCrewMember.vue
+++ frontend/src/components/RegisterCrewMember.vue
@@ -2,31 +2,74 @@
   <h1>Create a Crew Member Profile</h1>
   Details
   <div>
-  <form name="detailsForm">
+  <form v-if="!isConfirming" @submit.prevent="confirmInput" name="detailsForm">
       <label for="firstName">First Name:</label>
-      <input type="text" id="firstName" name="firstName" required>
+      <input type="text" id="firstName" name="firstName" required v-model="formData.firstName">
       <label for="lastName">Last Name:</label>
-      <input type="text" id="lastName" name="lastName" required> <br />
+      <input type="text" id="lastName" name="lastName" required v-model="formData.lastName"> <br />
       <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required>
+      <input type="email" id="email" name="email" required v-model="formData.email">
       <label for="phone">Phone Number:</label>
-      <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required> <br />
+      <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required v-model="formData.phone"> <br />
       <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
+      <input type="password" id="password" name="password" required v-model="formData.password">
       <label for="role">Role:</label>
-      <input type="text" id="role" name="role" required> <br />
+      <input type="text" id="role" name="role" required v-model="formData.role"> <br />
       <label for="qualifiedPos">Qualified Position:</label>
-      <input type="text" id="qualifiedPos" name="qualifiedPos" required> <br />
+      <input type="radio" id="sport-broadcasters" name="qualifiedPos" required value="Sport Broadcaster" v-model="formData.qualifiedPos">
+      <label for="sport-broadcasters">Sport Broadcaster</label> 
+      <input type="radio" id="paid-freelancers" name="qualifiedPos" required value="Paid Freelancer" v-model="formData.qualifiedPos">
+      <label for="paid-freelancers">Paid Freelancer</label>
+      <input type="radio" id="paid-students" name="qualifiedPos" required value="Paid Student" v-model="formData.qualifiedPos">
+      <label for="paid-students">Paid Student</label> <br />
       <input type="submit" value="Submit">
   </form>
+
+  <div v-if="isConfirming">
+    <h3>Confirm Your Information</h3>
+    <p>First Name: {{ formData.firstName }}</p>
+    <p>Last Name: {{ formData.lastName }}</p>
+    <p>Email: {{ formData.email }}</p>
+    <p>Phone Number: {{ formData.phone }}</p>
+    <p>Password: {{ formData.password }}</p>
+    <p>Role: {{ formData.role }}</p>
+    <p>Qualified Position: {{ formData.qualifiedPos }}</p>
+
+    <button @click="editInput">Edit</button>
+    <button @click="submitForm">Submit</button>
+  </div>
+
+
   </div>
 
 </template>
+
 <script setup>
+import { reactive, ref } from 'vue';
 
+const formData = reactive({
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  password: '',
+  role: '',
+  qualifiedPos: ''
+});
 
+const isConfirming = ref(false);
 
+function confirmInput() {
+  isConfirming.value = true;
+}
 
+function editInput() {
+  isConfirming.value = false;
+}
+
+function submitForm() {
+  console.log('Form submitted', formData);
+}
 </script>
 
 
