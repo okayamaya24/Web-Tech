c58e194a3d025f3279d7af75a3dd466a37816c29
okayamaya24 <amaya.harris@tcu.edu>
2025-04-14 18:37:02 -0500

    Create AdminView.vue

---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="admin-view">
+      <h1>Welcome, Admin</h1>
+      <AdminInviteForm />
+      <!-- You can also add AdminCrewList, ScheduleBuilder, etc. here later -->
+    </div>
+  </template>
+  
+  <script setup>
+  import AdminInviteForm from '../components/AdminInviteForm.vue';
+  </script>
