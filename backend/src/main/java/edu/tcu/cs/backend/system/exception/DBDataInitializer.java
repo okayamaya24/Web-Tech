a24ef3bf636c678a0f6eb8b7fee72338f0de34ae
okayamaya24 <amaya.harris@tcu.edu>
2025-04-24 19:50:29 -0500

    Update DBDataInitializer.java

--- backend/src/main/java/edu/tcu/cs/backend/system/exception/DBDataInitializer.java
+++ backend/src/main/java/edu/tcu/cs/backend/system/exception/DBDataInitializer.java
@@ -1,4 +1,4 @@
-package edu.tcu.cs.backend.system;
+package edu.tcu.cs.backend.system.exception;
 
 import edu.tcu.cs.backend.game.Game;
 import edu.tcu.cs.backend.game.GameRepository;
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
 
 @Component
 public class DBDataInitializer {
@@ -21,21 +22,24 @@ public class DBDataInitializer {
     public void init() {
         if (gameRepository.count() == 0) {
             Game g1 = new Game();
-            g1.setGameDate(LocalDate.of(2025, 4, 12));
-            g1.setGameTime(LocalTime.of(18, 30));
-            g1.setOpponent("Dallas Knights");
-            g1.setVenue("TCU Stadium");
-            g1.setRequiredCrewPositions("Referee, Line Judge");
-
-            Game g2 = new Game();
-            g2.setGameDate(LocalDate.of(2025, 4, 15));
-            g2.setGameTime(LocalTime.of(16, 0));
-            g2.setOpponent("Fort Worth Flames");
-            g2.setVenue("Panther Field");
-            g2.setRequiredCrewPositions("Scorekeeper, Referee");
+            g1.setSport("Football"); // THIS LINE!
+            g1.setGameDate(LocalDate.parse("2025-04-15"));
+            g1.setGameTime(LocalTime.parse("16:00"));
+            g1.setVenue("Test Stadium");
+            g1.setOpponent("Test Opponent");
+            g1.setRequiredCrewPositions(Arrays.asList("Camera", "Announcer"));
 
             gameRepository.save(g1);
+        }
+
+            Game g2 = new Game();
+            g2.setSport("Soccer");
+            g2.setGameDate(LocalDate.parse("2025-05-01"));
+            g2.setGameTime(LocalTime.parse("18:30"));
+            g2.setVenue("Panther Stadium");
+            g2.setOpponent("SMU");
+            g2.setRequiredCrewPositions(Arrays.asList("Producer", "Scorekeeper"));
+
             gameRepository.save(g2);
         }
     }
-}
