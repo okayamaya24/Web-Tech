efbea0aedc5858a0783cea0fcbcdfe2a9782f4d1
okayamaya24 <amaya.harris@tcu.edu>
2025-04-27 23:19:19 -0500

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,85 +2,39 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-  path: '/register',
-  name: 'RegisterCrewMember',
-  component: () => import('../components/RegisterCrewMember.vue'),
-  props: route => ({ token: route.query.token })
-},
-
-  { path: '/profile/:email?',
-    name: 'viewCrewProfile', 
-    component: () => import('@/view/ViewCrewProfile.vue'),
-    props: true
+    path: '/register',
+    name: 'RegisterCrewMember',
+    component: () => import('@/components/RegisterCrewMember.vue')
   },
-
-  {
-    path: '/admin',
-    name: 'AdminView',
-    component: () => import('../views/AdminView.vue')
-  },
-
   {
     path: '/login',
     name: 'login',
-    component: () => import('../components/Login.vue')
+    component: () => import('@/components/Login.vue')
   },
   {
-    path: '/availability',
-    name: 'availability',
-    component: () => import('../components/AvailabilityForm.vue')
-  },
-  {
-    path: '/crew',
-    name: 'crew',
-    component: () => import('../components/CrewList.vue')
-  },
-  {
-    path: '/viewGameSchedule',
-    name: 'viewGameSchedule',
-    component: () => import('../components/GameForm.vue')
+    path: '/admin',
+    component: () => import('@/components/AdminDashboard.vue'), //  Admin
+    children: [
+      { path: '', name: 'AdminView', component: () => import('@/views/AdminView.vue') },
+      { path: 'inviteCrewMember', name: 'inviteCrewMember', component: () => import('@/components/AdminInviteForm.vue') },
+      { path: 'createGameSchedule', name: 'createGameSchedule', component: () => import('@/components/ViewGameSchedule.vue') },
+      { path: 'manageGameSchedule', name: 'manageGameSchedule', component: () => import('@/components/AdminAddsGames.vue') },
+      { path: 'deleteCrew', name: 'deleteCrew', component: () => import('@/components/AdminDeleteCrew.vue') },
+      { path: 'viewCrew', name: 'viewCrew', component: () => import('@/components/AdminCrewList.vue') },
+      { path: 'scheduleCrew', name: 'scheduleCrew', component: () => import('@/components/CrewAssignment.vue') }
+    ]
   },
   {
     path: '/',
-    name: 'home',
-    component: () => import('../components/Home.vue'),
-  },
-  {
-    path: '/createGameSchedule',
-    name: 'createGameSchedule',
-    component: () => import('../components/ViewGameSchedule.vue')
-  },
-  {
-    path: '/manageGameSchedule',
-    name: 'manageGameSchedule',
-    component: () => import('../components/AdminAddsGames.vue')
-  },
-  {
-    path: '/inviteCrewMember',
-    name: 'inviteCrewMember',
-    component: () => import('../components/AdminInviteForm.vue')
-  },
-  {
-    path: '/dashboard',
-    name: 'dashboard',
-    component: () => import('../components/Dashboard.vue')
-  },
-  {
-    path: '/scheduleCrew',
-    name: 'scheduleCrew',
-    component: () => import('../components/CrewAssignment.vue')
-   },
-   {
-    path: '/deleteCrew',
-    name: 'deleteCrew',
-    component: () => import('../components/AdminDeleteCrew.vue')
-   },
-   {
-    path: '/viewCrew',
-    name: 'viewCrew',
-    component: () => import('../components/AdminCrewList.vue')
-   }
-  
+    component: () => import('@/components/CrewDashboard.vue'), // Crew
+    children: [
+      { path: '', name: 'home', component: () => import('@/components/Home.vue') },
+      { path: 'viewGameSchedule', name: 'viewGameSchedule', component: () => import('@/components/GameForm.vue') },
+      { path: 'crew', name: 'crew', component: () => import('@/components/CrewList.vue') },
+      { path: 'availability', name: 'availability', component: () => import('@/components/AvailabilityForm.vue') },
+      { path: 'profile/:email', name: 'viewCrewProfile', component: () => import('@/views/ViewCrewProfile.vue') }
+    ]
+  }
 ]
 
 const router = createRouter({
