03bc53760e7d20eb31b543c062446af7e875352f
okayamaya24 <amaya.harris@tcu.edu>
2025-04-27 23:20:05 -0500

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-//import App from './App.vue'
+import App from './App.vue'
 import router from './router'
-//import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 //import App from './components/ViewGameSchedule.vue'
 //import App from './components/AdminAddsGames.vue'
 //import App from './components/Login.vue'
-import App from './components/Dashboard.vue'
+//import App from './components/Dashboard.vue'
 //import App from './components/RegisterCrewMember.vue'
 //import App from './components/CrewAssignment.vue'
 
