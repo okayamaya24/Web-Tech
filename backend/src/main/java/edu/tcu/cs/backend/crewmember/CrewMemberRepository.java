594248ca86f31675e0ac59cd8ba826ad7582380a
okayamaya24 <amaya.harris@tcu.edu>
2025-04-27 20:36:26 -0500

    Update CrewMemberRepository.java

--- backend/src/main/java/edu/tcu/cs/backend/crewmember/CrewMemberRepository.java
+++ backend/src/main/java/edu/tcu/cs/backend/crewmember/CrewMemberRepository.java
@@ -1,10 +1,13 @@
 package edu.tcu.cs.backend.crewmember;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface CrewMemberRepository extends JpaRepository<CrewMember,Long> {
-    boolean existsByEmail(String email);
+@Repository
+public interface CrewMemberRepository extends JpaRepository<CrewMember, Long> {
+
+    boolean existsByEmail(String email); // You already have this one
     Optional<CrewMember> findByEmail(String email);
 }
