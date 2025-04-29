40c429d253dbbdd4d7955b82863ca793606e9883
okayamaya24 <amaya.harris@tcu.edu>
2025-04-28 19:01:07 -0500

    Delete backend/src/main/java/edu/tcu/cs/backend/crew assignment directory

--- backend/src/main/java/edu/tcu/cs/backend/crew assignment/CrewAssignmentRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.tcu.cs.backend.crewassignment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CrewAssignmentRepository extends JpaRepository<CrewAssignment, Long> {
-    List<CrewAssignment> findByGameId(Long gameId);
-}
